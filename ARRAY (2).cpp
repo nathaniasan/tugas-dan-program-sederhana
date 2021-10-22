a805cc3f0eac9ef19500a656bdeae4b0da48140d
Tan <89388944+nathaniasan@users.noreply.github.com>
2021-10-22 19:13:52 +0700

    Add files via upload

---
+++ ARRAY (2).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+	
+	int rata, jumlah;
+	int n=5;
+	int a [n]= {100, 90, 100, 80, 80};
+	
+	for (int i=0;i<5;i++){
+		cout<< a [i]<< endl;
+		
+		jumlah+=a[i];
+	}
+	
+	rata = jumlah/n;
+	
+	cout << "Rata Rata: "<<rata;
+	return 0;
+}
+
+// Nathania Santa Nigel Simbolon
+// A
+// 2117051009
